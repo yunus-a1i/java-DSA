71d8210c6704c017b531706d293bc885e92f80f7
Yunus Ali Siddiqui <yunusalisiddiqui90@gmail.com>
2023-12-11 21:08:40 +0530

    Sliding Window
    Fixed/Static sliding window

---
+++ slidingWindow.java
@@ -0,0 +1,16 @@
+public slidingWindow {
+    public static void main(String[] args){
+        int arr[]={100,300,400,500};
+        int k=2;
+        int n=4,sum=0,csum=0;
+        for(int i=0;i<k;i++){
+            sum+=arr[i];
+        }
+        csum=sum;
+        for(int i=k;i<n;i++){
+            csum+=(arr[i] - arr[i-k]);
+            sum = Math.max(sum,csum);
+        }
+        System.out.println(sum);
+    }
+}
