8d0f88f8f381f98444de40141392443f631c6ad2
Yunus Ali Siddiqui <yunusalisiddiqui90@gmail.com>
2025-03-02 21:54:00 +0530

    Create hollowPattern.java

---
+++ pattern/hollowPattern.java
@@ -0,0 +1,11 @@
+package pattern;
+
+import java.util.*;
+
+//hollow pattern
+
+public class hollowPattern {
+    public static void main(String[] args) {
+        System.out.println("red");     
+    }   
+}
